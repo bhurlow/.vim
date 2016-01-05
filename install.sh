be2c55217523298aa118bf0e92a54c7a65a2d2eb
bhurlow <bhurlow@gmail.com>
2016-01-04 20:40:53 -0800

    add install script instead of repos

---
+++ install.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+pushd ~/.vim/bundle
+
+packages=(
+ ctrlpvim/ctrlp.vim
+ easymotion/vim-easymotion
+ ervandew/supertab
+ guns/vim-clojure-static
+ dag/vim-fish
+ tpope/vim-repeat
+ tpope/vim-surround
+ mattn/emmet-vim
+ godlygeek/tabular
+ tpope/vim-commentary
+ tpope/vim-fireplace
+ bhurlow/vim-parinfer
+ tpope/vim-unimpaired
+ tpope/vim-speeddating
+)
+
+for x in "${packages[@]}"
+do
+  echo $var
+  git clone git@github.com:$x.git
+done
+
